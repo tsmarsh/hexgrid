164ae1d27f5dd8c8b02303a54b8ce0a50984f9c2
Tom Marsh <ts.marsh@gmail.com>
2015-07-22 17:41:57 -0400

    Updating the legal stuff

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 (defproject hexgrid-clj "0.1.0-SNAPSHOT"
   :description "Hexgrid utility functions for clojure"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "http://tailoredshapes.com"
+  :license {:name "BSD 2-Clause"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [prismatic/schema "0.4.3"]
                  [org.clojure/math.numeric-tower "0.0.4"]]
